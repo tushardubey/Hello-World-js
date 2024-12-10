a0eb6a3b8ea20c367bd16c27135aadbf74f2e902
tushardubey <71063095+tushardubey@users.noreply.github.com>
2024-12-10 17:50:19 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+// test.js
+const assert = require('assert');
+
+describe('Simple Test', function() {
+    it('should return true', function() {
+        assert.strictEqual(true, true);
+    });
+});
