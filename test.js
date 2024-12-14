ba00b50413a3377cb27aadf3dcf39dc65486acd6
tushardubey <71063095+tushardubey@users.noreply.github.com>
2024-12-14 16:06:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('Sample Test', () => {
-    it('should pass this test', () => {
-        expect(2 + 2).to.equal(4);
+describe('Sample Test', function () {
+    it('should pass', function () {
+        expect(true).to.be.true;
     });
 });
