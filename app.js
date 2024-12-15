46c388c80cb57465f0a9ee3e1ae6b12dec5ae94a
tushardubey <71063095+tushardubey@users.noreply.github.com>
2024-12-15 14:24:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// app.js
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Tushar!');
+  res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
